d19e817c6af1b9a7365e3bd56e26f83b6832e041
Fábio Barbosa <fabio@fabiobarbosa.com>
2025-06-17 16:27:39 +0200

    So we add the hardware back?

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
           nixos-hardware.nixosModules.lenovo-thinkpad-t480
           #nix-flatpak.nixosModules.nix-flatpak 
   
-          ./hosts/thinkpad-t480/hardware-configuration.nix
           ./hosts/thinkpad-t480/configuration.nix
           #./modules/flatpak.nix
         ];
