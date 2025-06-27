85ef1471e82feb243030d352d266ed68e1a7e48b
Fábio Barbosa <fabio@fabiobarbosa.com>
2025-06-27 09:52:07 +0200

    Let's simplify this a bit

--- flake.nix
+++ flake.nix
@@ -2,18 +2,11 @@
   description = "NixOS configuration with flakes";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    flake-utils.url = "github:numtide/flake-utils";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixos-hardware, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        # Other devShells or packages
-      }) // {
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
       nixosConfigurations.marshmallow = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
